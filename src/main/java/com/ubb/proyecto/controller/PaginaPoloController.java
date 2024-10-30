fcc7c3839c9e652bad28e44d079644a534398f5f
Joaco202 <joakocontreras06@gmail.com>
2024-10-30 19:43:23 -0300

    HU externo 6,número de teléfono para poder comunicarme con Polo

--- src/main/java/com/ubb/proyecto/controller/PaginaPoloController.java
+++ src/main/java/com/ubb/proyecto/controller/PaginaPoloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class PaginaController {
+public class PaginaPoloController {
 
     @GetMapping("/")
     public String inicio() {
