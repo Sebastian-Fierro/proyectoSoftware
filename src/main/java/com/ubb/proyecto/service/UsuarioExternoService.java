c8c20f32c3d095e256f7ca5eea63839afc0cb469
Joaco202 <joakocontreras06@gmail.com>
2024-10-30 17:25:09 -0300

    cambios menores

--- src/main/java/com/ubb/proyecto/service/UsuarioExternoService.java
+++ src/main/java/com/ubb/proyecto/service/UsuarioExternoService.java
@@ -1,7 +1,7 @@
 package com.ubb.proyecto.service;
 
 import java.util.List;
-/*import java.util.Optional;*/
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
